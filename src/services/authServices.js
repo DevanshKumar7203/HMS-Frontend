1c87975b5e1a4fdc0440680ead67497061d9bfba
Devansh <kumardevanshverma2003@gmail.com>
2026-04-30 11:54:30 +0530

    vercel

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -4,3 +4,13 @@ export const API = axios.create({
   baseURL: "https://hms-backend-1-s3ja.onrender.com/api",
  withCredentials: true,
 });
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
